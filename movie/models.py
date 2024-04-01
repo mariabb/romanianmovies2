ec6195c86869aed0968adc38ef17987d027c1678
mariabb <mariabufnea@gmail.com>
2024-04-01 17:29:51 +0300

    database changed 1

--- movie/models.py
+++ movie/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='movie/images/')
     url = models.URLField(blank=True)
     
 class Review(models.Model):    
 
-    text = models.CharField(max_length=100)    
+    text = models.CharField(max_length=1000)    
     date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
